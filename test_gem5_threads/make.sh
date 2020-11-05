2a0b6a75a5ab67451eb5b858307ef88ab4e42941
Yang Zhou <yangzhou@g.harvard.edu>
2020-11-04 22:44:29 -0500

    purge unused files

--- test_gem5_threads/make.sh
+++
@@ -1,6 +0,0 @@
-# !/bin/bash
-
-for i in test_file
-do
-    arm-linux-gnueabi-gcc -o $i $i.c --static -O3
-done
--- test_gem5_threads/test_file.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void main(){
-    FILE *file = fopen("/users/yangzhou/NF-GEM5/test.dat", "w");
-    fprintf(file, "hello gem5\n");
-    return;
-}
