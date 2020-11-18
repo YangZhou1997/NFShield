35252046d02e19e8b3c1546d61d27cc54d70ea69
YangZhou1997 <yangzhou@g.harvard.edu>
2020-11-17 23:31:13 -0500

    connecting two vms

--- run_qemu2.sh
+++ run_qemu2.sh
@@ -11,6 +11,6 @@ sudo qemu-system-riscv64 -M virt \
     -append "root=/dev/vda ro console=ttyS0" \
     -drive file=output/images/rootfs.ext2,format=raw,id=hd0 \
     -device virtio-blk-device,drive=hd0 \
-    -device virtio-net-device,netdev=net0,mac=DE:AD:BE:EF:7F:45 \
+    -device virtio-net-device,netdev=net0,mac=DE:AD:BE:EF:B7:90 \
     -netdev tap,id=net0,script=/home/vagrant/NF-GEM5/qemu-ifup \
     -nographic -m 1G
