096be80380f00acbf8f1d9aaf8e3921dae91d824
Cloud User <centos@ip-192-168-1-10.eu-west-1.compute.internal>
2022-02-07 05:16:29 +0000

    adding test_bw

--- sync_rootfs.sh
+++ sync_rootfs.sh
@@ -1,4 +1,8 @@
 # !/bin/bash
 source ~/chipyard/env.sh
-make clean && make -j
-rsync -rv --exclude=.git --max-size=500m ~/NF-GEM5 ~/firesim/sw/firesim-software/example-workloads/example-fed/overlay/root/
+cd ~/NF-GEM5 && make clean && make -j
+# rsync -rv --exclude=.git --max-size=500m ~/NF-GEM5 ~/firesim/sw/firesim-software/example-workloads/example-fed/overlay/root/
+rsync -rv --exclude=.git --max-size=500m ~/NF-GEM5 ~/firesim/sw/firesim-software/boards/firechip/base-workloads/fedora-base/overlay/root/
+
+cd ~/chipyard-lnic/tests-icenic && make clean && make -j
+rsync -rv --exclude=.git --max-size=500m ~/chipyard-lnic/tests-icenic ~/firesim/sw/firesim-software/boards/firechip/base-workloads/fedora-base/overlay/root/
